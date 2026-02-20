b3830e4eeebd0db96071e7c2df927d89bbf1afd4
sania bhutiani <saniabhutiani@gmail.com>
2026-02-20 22:07:58 +0530

    fix image url

--- src/utils/image.jsx
+++ src/utils/image.jsx
@@ -1,5 +1,9 @@
 export const getImageUrl = (path) => {
   if (!path) return "/no-image.png";
+
+  // If already full URL (Cloudinary etc)
   if (path.startsWith("http")) return path;
-  return path; 
+
+  // IMPORTANT — Backend live URL
+  return `https://vr-real-estate-backend.vercel.app/${path}`;
 };
